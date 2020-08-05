a2ab9b572197aefb1eaffaa6ef890de993a6e816
Tolkyn <wave_shtt@mail.ru>
2020-08-05 15:50:34 +0600

    Add files via upload

---
+++ gs.dbo.ref_contract_cancel.py
@@ -0,0 +1,46 @@
+import requests
+import pyodbc
+
+conn = pyodbc.connect(driver="SQL Server Native Client 11.0",
+                       host="10.10.101.4", database="GS",
+                       uid="sa", pwd="P@ssw0rd@2020", autocommit=True)
+
+def GS_DBO_1():
+    token = ('Bearer' + ' ' + 'f309ecc15d574833c45713da702049e2')
+    headers_1 = { 'Content-Type' : 'application/json','Authorization':'none' }
+    headers_1['Authorization'] = token
+
+    next_page = 'none'
+    URL_n = 'https://ows.goszakup.gov.kz/v3/refs/ref_contract_cancel'
+
+# for checking new code
+#     r_n = requests.get(URL_n, headers=headers_1, verify=False).json()
+#     print(type(r_n['items'][0]['id']))
+#     print(type(r_n['items'][0]['name_kz']))
+#     print(r_n)
+
+    while next_page != '':
+        r_n = requests.get(URL_n, headers=headers_1, verify=False).json()
+        next_page = r_n['next_page']
+        item = r_n['items']
+        for i in item:
+            print()
+            query = 'insert into gs.dbo.ref_contract_cancel( item_id, item_name_ru , item_name_kz) values (?,?,?)'
+            args = (i['id'], i['name_ru'], i['name_kz'])
+            print('this line is done!')
+            conn.execute(query, args)
+            print('end of round')
+        URL_n = 'https://ows.goszakup.gov.kz' + next_page
+    print('end of iteration ')
+
+GS_DBO_1()
+
+
+
+
+
+
+
+
+
+
